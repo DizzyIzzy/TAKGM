8d29adcb9be4d6c1532a5dadcbc151cfa985ee0b
Jackson Trudel <jtrudel97@gmail.com>
2021-08-10 19:23:56 -0400

    Adding UDPReceiver scripts and actor

---
+++ Source/TAKGM/Public/UDP/UDPReceiver.h
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Networking.h"
+#include "GameFramework/Actor.h"
+#include "Containers/UnrealString.h"
+#include "UDPReceiver.generated.h"
+
+UCLASS()
+class AUDPReceiver : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+		//====================================================
+		//		Data Received Events!
+public:
+	/** Data has been received!! */
+	UFUNCTION(BlueprintImplementableEvent)
+		void BPEvent_DataReceived(const FString& ReceivedData);
+
+	//====================================================
+
+public:
+	FSocket* ListenSocket;
+
+	FUdpSocketReceiver* UDPReceiver = nullptr;
+	void Recv(const FArrayReaderPtr& ArrayReaderPtr, const FIPv4Endpoint& EndPt);
+
+	UFUNCTION(BlueprintCallable, Category = "UDP")
+	bool StartUDPReceiver(
+		const FString& YourChosenSocketName,
+		const FString& TheIP,
+		const int32 ThePort
+	);
+
+	//ScreenMsg
+	FORCEINLINE void ScreenMsg(const FString& Msg)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, *Msg);
+	}
+	FORCEINLINE void ScreenMsg(const FString& Msg, const float Value)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("%s %f"), *Msg, Value));
+	}
+	FORCEINLINE void ScreenMsg(const FString& Msg, const FString& Msg2)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("%s %s"), *Msg, *Msg2));
+	}
+
+
+public:
+
+	/** Called whenever this actor is being removed from a level */
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+};
