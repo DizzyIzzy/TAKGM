aa9f6a031be5ff23e463daa94eba6062df61346b
Brian <brianksimpsonscholar@gmail.com>
2021-11-10 15:43:51 -0500

    Fixed New Cesium Version

--- Plugins/CesiumForUnreal/Source/CesiumRuntime/Private/CesiumCreditSystemBPLoader.cpp
+++
@@ -1,9 +0,0 @@
-#include "CesiumCreditSystemBPLoader.h"
-#include "UObject/ConstructorHelpers.h"
-
-UCesiumCreditSystemBPLoader::UCesiumCreditSystemBPLoader()
-    : CesiumCreditSystemBP(
-          ConstructorHelpers::FObjectFinder<UClass>(
-              TEXT(
-                  "Class'/CesiumForUnreal/CesiumCreditSystemBP.CesiumCreditSystemBP_C'"))
-              .Object) {}
